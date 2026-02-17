0f4dee2e910feeaba7d6583f3e6635d5829aa31d
Paolo Marzocchi <paolomarzocchi3@gmail.com>
2026-02-17 09:58:37 +0100

    add authorize method to customer form requests

--- backend/app/Http/Requests/Customer/StoreCustomerRequest.php
+++ backend/app/Http/Requests/Customer/StoreCustomerRequest.php
@@ -5,10 +5,19 @@ namespace App\Http\Requests\Customer;
 use App\Models\Customer;
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class StoreCustomerRequest extends FormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
